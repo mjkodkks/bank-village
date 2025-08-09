41c65d40952d166f3857449e55a8384080d38615
DKKs <eye.q.21@gmail.com>
2025-08-10 03:10:51 +0700

    [Add] new interest calculate

--- web-client/composables/useInterest.ts
+++ web-client/composables/useInterest.ts
@@ -1,8 +1,8 @@
 function useInterest() {
   const tempalteMonth = {
-    SAVING: 0.005,
-    STOCK: 0.0075,
-    LOAN: 0.0125,
+    SAVING: 5 / 100 / 12, // 5% per year, divided by 12 months
+    STOCK: 8 / 100 / 12, // 8% per year, divided by 12 months
+    LOAN: 15 / 100 / 12, // 1.25% per year, divided by 12 months
   }
 
   function calInterestByType(value: number, accountType: AccountType): number {
